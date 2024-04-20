48cac35cfef9923b79117c6b16d715a4e5a294cb
nousantx <nousantx@gmail.com>
2024-04-20 16:35:48 +0700

    feat: Update initial code

--- dist/output.js
+++
@@ -1,9 +0,0 @@
-const styles = {
-  ".flex": "d-flex",
-  ".justify-center": "jc-center",
-  ".items-center": "ai-center",
-  ".font-thin": "fw-100",
-  ".rounded": "br-0.25rem",
-  ".bg-top": "bg-loc-top"
-};
-makeStyles(styles)
